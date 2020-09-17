6a5970f5c047f1db03a43740871a0e1472d737a4
Jonathan Sutcliffe <jonathan.sutcliffe@gov.scot>
2020-09-17 16:02:41 +0100

    Alter tracking for checkboxes; remove tracking for selects

--- src/tracking.js
+++ src/tracking.js
@@ -1,16 +1,11 @@
 const tracking = {
     init: function () {
-        const checkboxes = [].slice.call(document.querySelectorAll('[type="checkbox"]'));
+        const checkboxes = [].slice.call(document.querySelectorAll('.ds_checkbox__input[type="checkbox"]'));
         checkboxes.forEach(checkbox => {
-            checkbox.addEventListener('change', (event) => {
-                checkbox.dataset.form = `checkbox-${event.target.id}-${event.target.checked ? 'checked' : 'unchecked'}`;
-            });
-        });
+            const label = document.querySelector(`[for=${checkbox.id}]`);
 
-        const selects = [].slice.call(document.querySelectorAll('select'));
-        selects.forEach(select => {
-            select.addEventListener('change', (event) => {
-                select.dataset.form = `select-${event.target.id}-${event.target.value}`;
+            label.addEventListener('click', () => {
+                checkbox.dataset.form = `checkbox-${checkbox.id}-${checkbox.checked ? 'unchecked' : 'checked'}`;
             });
         });
     }
